356365a4ac37ed952b6bbc7e5ccff6e96f95cf25
NASa <sonyspawn@163.com>
2016-01-15 11:04:15 +0800

    Finish parsing namespace

--- json2cpp.py
+++ json2cpp.py
@@ -15,7 +15,8 @@ rapidjson_path = "rapidjson"
 ######################################## file       template    ####################################
 
 FILE_HEADER = '''#include "base.h"
-namespace json2cpp{
+
+using namespace json2cpp;
 
 '''
 
@@ -874,19 +875,16 @@ def load_grammar():
     return OneOrMore(grammar).ignore(cppStyleComment)
 
 
-def parse_namspace(token, base_dir):
-    namespace_str = ""
+def parse_namespace(token, base_dir):
+    namespace_list = []
     base_dir_str = base_dir
     for i in range(len(token)):
         if i == 0:
             pass
-        elif i == 1:
-            namespace_str = token[1]
-            base_dir_str = base_dir_str + os.sep + token[1]
         else:
-            namespace_str += "::" + token[i]
+            namespace_list.append(token[i])
             base_dir_str = base_dir_str + os.sep + token[i]
-    return namespace_str, base_dir_str
+    return namespace_list, base_dir_str
 
 
 def parse_class(class_token):
@@ -1110,7 +1108,16 @@ def generate_interface(base_directory, namspace_str, class_fields, interface):
     for classField in class_fields:
         class_str += classField.dump()
 
-    header = FILE_HEADER + class_str + interface.dump + FILE_FOOTER
+    ns_str = ""
+    file_footer = "\n"
+    # handle namespace
+    if namspace_str:
+        for nameS in namspace_str:
+            ns_str = ns_str + "namespace " + nameS + " { "
+            file_footer += "} "
+        ns_str += "\n"
+    print ns_str
+    header = FILE_HEADER + ns_str + class_str + interface.dump + file_footer
 
     header_h = open(base_directory + os.sep + interface.name + ".h", "w")
     header_h.write(header)
@@ -1127,7 +1134,7 @@ def generate_files(tokens, base_dir):
     print tokens
     print "-----------------"
     class_fields = []
-    namespace_str = ""
+    namespace = ""
     for token in tokens:
         print token
         if (type(token[0]) == list and token[1] == "class") or token[0] == "class":
@@ -1139,14 +1146,18 @@ def generate_files(tokens, base_dir):
             interface = parse_interface(token)
         elif token[0] == "namespace":
             print "Parse  namespace..."
-            namespace = parse_namspace(token, base_dir)
+            namespace = parse_namespace(token, base_dir)
         else:
             print "[ERROR] Parsing token failed! No class or Interface key word find!"
             return
 
-    base_directory = ""
+    base_directory = base_dir
+    namespace_str = ""
     if namespace != "":
+        namespace_str = namespace[0]
+        print namespace_str
         base_directory = namespace[1]
+        print base_directory
     # Create dir by namespace
     if not os.path.exists(base_directory):
         try:
@@ -1154,7 +1165,7 @@ def generate_files(tokens, base_dir):
         except OSError, why:
             print u"[错误]创建目标文件夹." + os.path.abspath(base_directory) + u"失败."
             exit(-1)
-    generate_interface(base_directory, namespace[0], class_fields, interface)
+    generate_interface(base_directory, namespace_str, class_fields, interface)
     str_out = MACRO_H
     for req_macros in request_iter_marcos_file:
         str_out += request_iter_marcos_file[req_macros]
