73de0ae8e7fedf21fb6ad538bcada6d056293b39
NASa <sonyspawn@163.com>
2016-01-19 17:56:39 +0800

    Add support for jsoncpp

---
+++ test_with_JsonCPP.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+git clone https://github.com/open-source-parsers/jsoncpp.git
+cd jsoncpp/
+mkdir -p build/debug
+cd build/debug
+cmake -DCMAKE_BUILD_TYPE=debug -DBUILD_STATIC_LIBS=ON -DBUILD_SHARED_LIBS=OFF -DARCHIVE_INSTALL_DIR=. -G "Unix Makefiles" ../..
+make
+cd ../../
+mkdir lib
+cp build/debug/src/lib_json/libjsoncpp.a lib/
+cd ..
+./json2cpp.py jsoncpp sample.jsf test
+cd ../test/test/
+cp ../Makefile.jsoncpp ./Makefile
+make & ./test
